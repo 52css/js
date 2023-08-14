9bc6658549c122029a420656b30d770826912fc9
wèijié <weijie@rongyi.com>
2023-08-14 16:15:01 +0800

    两数之和 - 带说明

--- twoNumAdd.js
+++ twoNumAdd.js
@@ -1,3 +1,11 @@
+/**
+两数之和
+题目： 给定一个数组 nums 和一个目标值 target，在该数组中找出和为目标值的两个数
+
+输入： nums: [8, 2, 6, 5, 4, 1, 3] ； target:7
+
+输出： [2, 5]
+ */
 // 时间复杂度O(n)、 空间复杂度O(n)
 function twoNumAdd(arr, target) {
   if (Array.isArray(arr)) {
