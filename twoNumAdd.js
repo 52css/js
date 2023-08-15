86bd1173e52357537cd7195c374d637b7b5e06c7
wèijié <weijie@rongyi.com>
2023-08-15 19:34:43 +0800

    两数之和增加注释

--- twoNumAdd.js
+++ twoNumAdd.js
@@ -9,10 +9,14 @@
 // 时间复杂度O(n)、 空间复杂度O(n)
 function twoNumAdd(arr, target) {
   if (Array.isArray(arr)) {
+    // 定义临时变量，用于村未加载的值
+    // map[8] = true
+    // map[2] = true
+    // ...
+    // map[5] 需要判断 map[target - 5] 是否存在，存在就直接返回
     const map = {};
-
-    for (let i = 0; i < arr.length; i++) {
-      let item = arr[i];
+    for (let i = 0; i < arr.length; i ++ ) {
+      const item = arr[i];
       if (map[target - item] !== undefined) {
         return [target - item, item];
       } else {
