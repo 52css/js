2a3e65e523f47b7b9d331e2f56efdd01549d8709
wèijié <weijie@rongyi.com>
2023-08-17 09:35:15 +0800

    字符串所有排列

--- stringGroup.js
+++ stringGroup.js
@@ -5,26 +5,25 @@
 输入： 'abc'
 
 输出： ['abc', 'acb', 'bca', 'bac', 'cab', 'cba']
-递归。abc的所有组合值可以分为以下部分：
+
+思路：
+abc的所有组合值可以分为以下部分：
 1. a+ 剩余字母组合值；
 2. b+ 剩余字母组合值；
 3. c+ 剩余字母组合值；
  */
 
 function stringGroup(str) {
-  if (str.length <= 1) {
-    return [str]
-  }
+  if (str.length <= 1) {return [str]}
   const result = [];
-  for (let i = 0; i < str.length; i ++) {
-    const item = str[i]
-    const notItem = str.slice(0, i) + str.slice(i + 1)
-    const notItemResult = stringGroup(notItem).map(x => item + x)
+  for (let i = 0; i < str.length ; i ++) {
+    const char = str[i];
+    const arr1 = stringGroup(str.slice(0, i) + str.slice(i + 1));
 
-    result.push(...notItemResult)
+    result.push(...arr1.map(x => char + x))
   }
 
-  return result
+  return result;
 }
 
 // 输出： ['abc', 'acb', 'bca', 'bac', 'cab', 'cba']
