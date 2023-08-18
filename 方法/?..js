4c7c66acd4074ea51f9203b60eca170ed0809af9
wèijié <weijie@rongyi.com>
2023-08-18 16:05:03 +0800

    ?.

--- 方法/?..js
+++ 方法/?..js
@@ -1,11 +1,13 @@
 /**
- * ?. 号的巧用
- * 链判断运算符（?.）允许读取位于连接对象链深处的属性的值，
- * 而不必明确验证链中的每个引用是否有效。
+ * ?. 的巧用
+ * 链判断运算符（?.）允许读取位于连接对象链深处的属性的值，而不必明确验证链中的每个引用是否有效。
  * 【代码示例】
  */
 var obj = {}
 
-var name1 = obj && obj.item && obj.item.name // 传统写法
+var name1 = obj && obj.test && obj.test.name;
 
-var name2 = obj?.item?.name // es11写法
+var name2 = obj?.test?.name;
+
+console.log(name1)
+console.log(name2)
