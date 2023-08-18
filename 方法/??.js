64b43ab084d53e0620425b199dd24f5e209ee2aa
wèijié <weijie@rongyi.com>
2023-08-18 10:01:26 +0800

    增加??

---
+++ 方法/??.js
@@ -0,0 +1,11 @@
+/**
+ * ?? 号的巧用
+ */
+// 数据判断
+var str1 = '0'
+var num1 = 0
+var val1 = str1 || 'hello' // 输出？
+var val2 = num1 || 'hello' // 输出？
+
+var val3 = str1 ?? 'hello' // 输出？
+var val4 = num1 ?? 'hello' // 输出？
