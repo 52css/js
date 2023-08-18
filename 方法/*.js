6647dc2fc16e62027b977a8b55c7c920c52dd718
wèijié <weijie@rongyi.com>
2023-08-18 16:55:33 +0800

    * 号

--- 方法/*.js
+++ 方法/*.js
@@ -1,15 +1,23 @@
 /**
  * * 号的巧用
- * 可以实现字符串转数字，代替日期.getSeconds()等
+ * 实现字符串转数字，代替Date.getTime()
  * 【代码示例】
+ * ** 号代替 Math.pow()
  */
 
 // 字符串转数字
 var str1 = '123'
 
-console.log(str1 * 1)
+console.log(str1 * 1, typeof(str1 * 1))
+
 
 // 代替日期.getSeconds()
 var date = new Date()
 
-console.log(date * 1)
+console.log(date * 1, date * 1 === date.getTime())
+
+
+// ** 的使用
+var num = 10 ** 2
+
+console.log(num === Math.pow(10, 2))
