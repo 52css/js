d312fe93b60af9f411b87d031b416378df7e6e03
wèijié <weijie@rongyi.com>
2023-09-08 23:12:02 +0800

    再完成部分

--- 搭框架/vue/pnpm.sh
+++ 搭框架/vue/pnpm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -19,3 +19,12 @@ cd vite-project
 # 安装ESLint + Prettier
 #pnpm add eslint-config-prettier eslint-plugin-prettier -D
 # echo "module.exports = {\n  \"env\": {\n    \"browser\": true,\n    \"es2021\": true\n  },\n  \"extends\": [\n    \"eslint:recommended\",\n    \"plugin:@typescript-eslint/recommended\",\n    \"plugin:vue/vue3-essential\",\n    \"plugin:prettier/recommended\"\n  ],\n  \"overrides\": [\n    {\n      \"env\": {\n        \"node\": true\n      },\n      \"files\": [\".eslintrc.{js,cjs}\"],\n      \"parserOptions\": {\n        \"sourceType\": \"script\"\n      }\n    }\n  ],\n  \"parserOptions\": {\n    \"ecmaVersion\": \"latest\",\n    \"parser\": \"@typescript-eslint/parser\",\n    \"sourceType\": \"module\"\n  },\n  \"plugins\": [\n    \"@typescript-eslint\",\n    \"vue\",\n    \"prettier\"\n  ],\n  \"rules\": {\n    \"prettier/prettier\": \"error\",\n    \"arrow-body-style\": \"off\",\n    \"prefer-arrow-callback\": \"off\"\n  }\n}"> .eslintrc.cjs
+
+# 安装Husky
+# pnpm add -D husky
+# npm pkg set scripts.prepare="husky install"
+# npm run prepare
+
+# 安装lint-staged
+# pnpm add -D lint-staged
+# npm pkg set lint-staged={"*.{ts,vue}": ["npm run lint", "prettier --write"]}
