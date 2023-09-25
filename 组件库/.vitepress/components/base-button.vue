5aef77cd873a1bf26d2229a0decf1e9f3f3e7af9
wèijié <weijie@rongyi.com>
2023-09-25 20:56:28 +0800

    增加的按钮

--- 组件库/.vitepress/components/base-button.vue
+++ 组件库/.vitepress/components/base-button.vue
@@ -1,35 +1,31 @@
 <script lang="ts">
 import { ref, defineProps } from 'vue'
 export interface BaseButtonProps {
-  onClick?: (event: Event) => void
+  onClick?: () => void
 }
 
 const isPromise = (obj: any) => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const AsyncFunction = (async () => {}).constructor;
-    return obj instanceof Promise || obj instanceof AsyncFunction === true;
-  };
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const AsyncFunction = (async () => {}).constructor;
+  return obj instanceof Promise || obj instanceof AsyncFunction === true;
+};
 
-export function useOnClick(props: BaseButtonProps, loading) {
+export function useOnClick(props, loading) {
   return () => {
-    const fn = props.onClick;
-    if (!fn || loading.value) {
-      return;
+    if (!props.onClick || loading.value) {
+      return
     }
 
-    loading.value = true;
-
-    const execFn = fn.call(null, event);
-
-    if (isPromise(execFn)) {
-      execFn.finally(() => {
-        loading.value = false;
-      });
+    loading.value = true
+    if (isPromise(props.onClick)) {
+      props.onClick().finally(() => {
+        loading.value = false
+      })
     } else {
-      // 延迟300ms，关闭loading
+      props.onClick()
       setTimeout(() => {
-        loading.value = false;
-      }, 300);
+        loading.value = false
+      }, 300)
     }
   }
 }
@@ -39,18 +35,16 @@ export default {
 }
 </script>
 <script setup lang="ts">
-const props = defineProps<BaseButtonProps>()
+const props = withDefaults(defineProps<BaseButtonProps>(), {})
 const loading = ref(false)
-console.log('props', props)
-const handleClick = useOnClick(props, loading)
+const onClick = useOnClick(props, loading)
 </script>
 
 <template>
-  <el-button :loading="loading" @click="handleClick">
+  <el-button :loading="loading" @click="onClick" v-bind="$attrs">
     <slot />
   </el-button>
 </template>
 
 <style scoped lang="scss">
 </style>
-
