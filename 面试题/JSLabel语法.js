4b43f8cd48f648d9ad5e906fb0a2d07c377152b8
wèijié <weijie@rongyi.com>
2024-02-18 15:35:37 +0800

    JSLabel语法

---
+++ 面试题/JSLabel语法.js
@@ -0,0 +1,10 @@
+outer: for (let i = 0; i < 10; i ++) {
+  console.log('顶层循环');
+  for (let j = 0; j < 10; j ++) {
+    console.log('内层循环');
+    if (i * j > 30) {
+      console.log('退出顶层循环');
+      break outer;
+    }
+  }
+}
