b0b17a036d05f8773210447bbb929d45be5991c5
wèijié <weijie@rongyi.com>
2024-02-18 10:07:17 +0800

    惰性函数

---
+++ 面试题/惰性函数.js
@@ -0,0 +1,16 @@
+function createCopyText() {
+  if (navigator.clipboard) {
+    return (text) => navigator.clipboard.writeText(text);
+  } else {
+    return (text) => {
+      const input = document.createElement("input");
+      input.setAttribute("value", text);
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand("copy");
+      document.body.removeChild(input);
+    };
+  }
+}
+
+const copyText = createCopyText()
