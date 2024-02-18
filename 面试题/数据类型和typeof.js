c891030c9edfce5588f29c74d7c1de7432db352f
wèijié <weijie@rongyi.com>
2024-02-18 15:03:25 +0800

    数据类型和typeof

---
+++ 面试题/数据类型和typeof.js
@@ -0,0 +1,11 @@
+// 原始类型              typeof
+// 1. Number            number
+// 2. String            string
+// 3. Boolean           boolean
+// 4. Null              object
+// 5. Undefined         undefined
+// 6. Symbol            symbol
+// 7. BigInt            bigint
+
+// 对象类型
+// 1. Object            object function
